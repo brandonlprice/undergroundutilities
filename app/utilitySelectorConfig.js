216246826e6fddf1f12eee7a959e5a7fa4ca611f
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2022-04-01 11:43:13 -0700

    Add files via upload

---
+++ app/utilitySelectorConfig.js
@@ -0,0 +1,9 @@
+﻿define(["dojo/_base/declare"], function (declare) {
+    return declare(null, {
+        utilitySelectorUrl: 'http://services1.arcgis.com/8CpMUd3fdw6aXef7/arcgis/rest/services/D7Highway/FeatureServer/0',
+        countyField: 'Cnty',
+        routeField: 'Route',
+        postmileField: 'DYNSEGPM',
+		postmileOrderByField: 'PM'
+    });
+});
